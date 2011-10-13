0b9367727d44d702e4cea84c7a14a8c60c245772
yiqing-95 <yiqing95@yahoo.com.cn>
2011-10-13 12:18:22 +0800

    modify the hard code Class name ScrollTop ,  use self key word to  instead of ScrollTop thus  you can easily modify the Class name such as  EScrollTop or whatever without touching the rest code .

--- ScrollTop.php
+++ ScrollTop.php
@@ -1,30 +1,55 @@
 <?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: yiqing
+ * Date: 11-10-13
+ * Time: 下午12:07
+ * To change this template use File | Settings | File Templates.
+ */
 
-class ScrollTop extends CWidget {
+class ScrollTop extends CWidget
+{
 
+    /**
+     * @var string
+     */
     private $id = 'ScrollTop';
+    /**
+     * @var string
+     */
     public $label = '^top';
+    /**
+     * @var string
+     */
     public $speed = 'slow';
-    public static $counter;
-    
-   
-     
-    public $linkOptions = array(); 
- 
+    /**
+     * @var int
+     */
+    public static $counter = 1;
 
 
-    public function init() {
-        ScrollTop::$counter++;
+    /**
+     * @var array
+     */
+    public $linkOptions = array();
+
+
+    /**
+     * @return void
+     */
+    public function init()
+    {
+        self::$counter++;
         Yii::app()->getClientScript()->registerScript($this->id + '_' + ScrollTop::$counter, '
                 $(function() {
-                    $("#' . ($this->id + '_' + ScrollTop::$counter) . '").click(function() {
+                    $("#' . ($this->id + '_' + self::$counter) . '").click(function() {
                         $("html,body").animate({ scrollTop : 0 }, "' . ($this->speed) . '");
                         return false;
                     });
                 });');
-                
-        echo CHtml::link($this->label,$url='#',CMap::mergeArray(array('id'=>$this->id),$this->linkOptions));
-       
+
+        echo CHtml::link($this->label, $url = '#', CMap::mergeArray(array('id' => $this->id), $this->linkOptions));
+
     }
 
 }
